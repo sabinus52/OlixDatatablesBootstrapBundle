3830abf907ff3dc7a243f66a981f5c4f11d22df9
Olivier <sabinus52@gmail.com>
2015-01-22 21:47:15 +0100

    Classe de personnalisation de "datatables" pour les tableaux au format bootstrap

---
+++ Datatable/AbstractDatatableView.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Olix\DatatablesBootstrapBundle\Datatable;
+
+use Sg\DatatablesBundle\Datatable\View\AbstractDatatableView as BaseAbstractDatatableView;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+
+abstract class AbstractDatatableView extends BaseAbstractDatatableView
+{
+
+    /**
+     * Bootstrap's table styling options.
+     *
+     * @var string
+     */
+    const BOOTSTRAP_OLIX_STYLE = "table table-striped";
+
+
+    /**
+     * Ctor.
+     *
+     * @param TwigEngine          $templating           The templating service
+     * @param TranslatorInterface $translator           The translator service
+     * @param RouterInterface     $router               The router service
+     * @param array               $defaultLayoutOptions The default layout options
+     */
+    public function __construct(TwigEngine $templating, TranslatorInterface $translator, RouterInterface $router, array $defaultLayoutOptions)
+    {
+        parent::__construct($templating, $translator, $router, $defaultLayoutOptions);
+        
+        $this->getOptions()->setPagingType('simple_numbers');
+        $this->setStyle(self::BOOTSTRAP_OLIX_STYLE);
+        $this->setUseIntegrationOptions(true);
+    }
+
+
+}
